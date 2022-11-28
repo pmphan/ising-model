0afb2deb608a562ab5a05c179a46a7862b586857
Phuong <pmphan00@gmail.com>
2022-11-27 22:25:43 -0500

    feat: Add triangle 2D lattice.

---
+++ controllers/calculation.py
@@ -0,0 +1,42 @@
+from flask import request
+from flask_socketio import Namespace, emit, disconnect
+
+from service.serialization import deserialize_array, serialize_plot
+from service.square2D import Square2D
+from service.triangle2D import Triangle2D
+
+class CalculationNamespace(Namespace):
+    disconnected_clients = set()
+
+    class_mapping = {
+        "square2D": Square2D,
+        "triangle2D": Triangle2D
+    }
+
+    def on_calculate(self, data):
+        params : dict[str, str] = {}
+        for kv in data:
+            params[kv['name']] = kv['value']
+        N = int(params['ndim'])
+        kT = float(params['ktemp'])
+        nstep = int(params['nstep'])
+        nyield = int(params['nimg'])
+        array = deserialize_array(params['array'])
+        Class2D = self.class_mapping[params['ns']]
+        for result, i in Class2D.metropolis(array, kT, nstep, nyield):
+            # Early termination
+            if request.sid in self.disconnected_clients:
+                break
+            title = f"{N}x{N} result at step {i} with kT={kT}"
+            splot = Class2D.plot_lattice(result, title, serialize_plot)
+            emit("calculating", {'i': i, 'plot': splot})
+
+        self.disconnected_clients.remove(request.sid)
+        emit("calculated")
+
+    def on_stop(self):
+        self.disconnected_clients.add(request.sid)
+
+    def on_disconnect(self):
+        disconnect()
+        self.disconnected_clients.add(request.sid)
--- controllers/triangle2D.py
+++ controllers/triangle2D.py
@@ -1,29 +1,29 @@
 from flask import Blueprint, render_template, request
 
-from service.square2D import Square2D
+from service.triangle2D import Triangle2D
 from service.serialization import serialize_array, serialize_plot
 
-sq2d_bp = Blueprint(
-    name='square2D',
+tri2d_bp = Blueprint(
+    name='triangle2D',
     import_name=__name__,
     static_folder='static',
-    template_folder='templates/square2D',
-    url_prefix='/square2D'
+    template_folder='templates/triangle2D',
+    url_prefix='/triangle2D'
 )
 
-@sq2d_bp.route("/")
+@tri2d_bp.route("/")
 def index():
-    return render_template("common2D.html", ns="square2D")
+    return render_template("common2D.html", ns="triangle2D")
 
-@sq2d_bp.route("/plot_lattice", methods=["POST"])
+@tri2d_bp.route("/plot_lattice", methods=["POST"])
 def plot_lattice():
     N = int(request.form.get('ndim', 2))
     kT = float(request.form.get('ktemp', 2.0))
-    lattice = Square2D(N, N, kT)
+    lattice = Triangle2D(N, kT)
     array = lattice.lattice
 
     title = f"2D Square Lattice {N}x{N}, kT={kT}"
-    serialized_plot = Square2D.plot_lattice(array, title, serialize_plot)
+    serialized_plot = Triangle2D.plot_lattice(array, title, serialize_plot)
 
     serialized_array = serialize_array(array)
 
